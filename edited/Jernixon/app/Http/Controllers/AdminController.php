60db6b7128ebace856d4020792a5b5c1f81ee8d3
marinelbaucas <marinelbaucas@gmail.com>
2018-03-15 18:42:23 +0800

    Edited the side navbar of the page

---
+++ edited/Jernixon/app/Http/Controllers/AdminController.php
@@ -0,0 +1,235 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Product;
+use App\User;
+use Datatables;
+use DB;
+class AdminController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        // $this->middleware('auth');
+        $this->middleware('auth:adminGuard');
+    }
+
+    public function dashboard()
+    {
+        // return view('admin');
+        return view('dashboard.dashboard');
+    }
+    public function sales()
+    {
+        // return view('admin');
+        return view('adminViews.sales');
+    }
+    public function purchases()
+    {
+        // return view('admin');
+        return view('adminViews.purchases');
+    }
+    public function returns()
+    {
+        // return view('admin');
+        return view('adminViews.returns');
+    }
+    public function physicalCount()
+    {
+        // return view('admin');
+        return view('adminViews.physicalCount');
+    }
+    public function items()
+    {
+        // $products = Product::paginate(2);
+        return view('items.index');
+        // return view('items.index')->with('products',$products);
+    }
+    public function reports()
+    {
+        return view('reports.index');
+
+    }
+    public function employees(){
+        $employees = User::all();
+
+        return view('pages.employees')->with('employees',$employees);
+    }
+
+    public function stockAdjustment()
+    {
+        // return view('admin');
+        return view('adminViews.stockAdjustment');
+    }
+
+    public function getItemsForSales(){
+
+        //  $data = DB::select("SELECT products.description, products.price as retail_price, purchases.price as wholesale_price, status FROM `products` join `purchases` using(product_id) where status='SALABLE' OR status='DAMAGED-SALABLE'");
+        //  while ( $rows = mysqli_fetch_assoc($data)  ) {
+        //     $description = $rows['description'];
+
+        //     $data1 = DB::select("SELECT products.price as retail_price FROM `products` join `purchases` using(product_id) where description="" AND status='SALABLE'");
+        //     $data2 = DB::select("SELECT COUNT(product_id) FROM `products` join `purchases` using(product_id) where description="" AND status='SALABLE'");
+        //     $data2 = DB::select("SELECT COUNT(product_id) FROM `products` join `purchases` using(product_id) where description="" AND status='DAMAGED-SALABLE'");
+        //  }
+
+        $data = DB::table('products')->select('*');
+        // return $data;
+
+        return Datatables::of($data)
+            ->addColumn('action',function($data){
+                return "
+                 <button class='btn btn-info' id='$data->product_id' onclick='addItemToCart(this)'><i class='glyphicon glyphicon-plus'></i></button></a>";
+
+
+            })
+            ->make(true);
+
+    }
+    public function getDataPoints(){
+
+    }
+
+
+
+    public function addQuantity(Request $request){
+        //update purchase set price='$newUnitCost', quantity='$newPurchase' WHERE item_id='$item_id' and price='$oldUnitCost' and quantity='$oldPurchase'"
+        //$name = $request->input('user.name'); 
+        //dd=(json_decode($request->getContent(), true));
+        //$data = $request->json()->all();
+        return "pending query...";
+        // $item = DB::select("UPDATE product set _='$request->input('inputValue')'");
+    }
+    public function subtractQuantity(Request $request){
+        // $item = DB::select("");        
+        return "pending query...";        
+    }
+    public function returnItem(Request $request){
+        $this->validate($request,[
+            'customerName' => 'required',
+            'itemName' => 'required',
+            'quantity' => 'required',
+            'totalPrice' => 'required',
+            'reason' => 'required',
+            'status' => 'required'
+        ]);
+        // $item = DB::select("");
+
+        return "pending query for return item...";        
+    }
+    public function getItemsForItems(){
+
+        $data = DB::table('products')->select('*');
+        return Datatables::of($data)
+            ->addColumn('action',function($data){
+                return "<a href = '#addModal' data-toggle='modal'>
+                    <button id='Add' class='btn btn-success' onclick='insertDataToModal(this)'><i class='glyphicon glyphicon-plus-sign'></i> Add</button>
+                </a>
+                <a href = '#subtractModal' data-toggle='modal' >
+                    <button id='Subtract'class='btn btn-danger' onclick='insertDataToModal(this)'><i class='glyphicon glyphicon-minus-sign'></i> Subtract</button>
+                </a>
+
+                <a href = '#removeModal' data-toggle='modal' >
+                    <button id='Remove' class='btn btn-danger'><i class='glyphicon glyphicon-remove'></i> Remove Item</button>
+                </a>
+                <a href = '#editModal' data-toggle='modal' >
+                    <button id='Edit' class='btn btn-info' onclick='insertDataToModal(this)'><i class='glyphicon glyphicon-edit'></i>Edit</button>
+                </a>
+
+                ";
+
+
+            })
+            ->make(true);
+    }
+    public function storeNewItem(Request $request){
+        $this->validate($request,[
+            'description' => 'required',
+            'quantityInStock' => 'required',
+            'wholeSalePrice' => 'required',
+            'retailPrice' => 'required'
+        ]);
+
+        //Create new Item
+        $item = new Product;
+        $item->description = $request->input('description');
+        //$item->quantityInStock = $request->input('quantityInStock');
+        $item->price = $request->input('wholeSalePrice');
+        //$item->retailPrice = $request->input('retailPrice');
+        $item->save();
+        return response($request->all());
+        // return "success";
+        // return redirect('/items')->with('success','Success adding item');
+    }
+
+
+    public function getTransactions(){
+        $data = DB::table('products')->select('*');
+        return Datatables::of($data)
+            ->make(true);
+    }
+    public function getReturns(){
+        $data = DB::table('products')->select('*');
+        return Datatables::of($data)
+            ->make(true);
+    }
+    public function getItemsAdded(){
+        $data = DB::table('products')->select('*');
+        return Datatables::of($data)
+            ->make(true);
+    }
+    public function getRemovedItems(){
+        $data = DB::table('products')->select('*');
+        return Datatables::of($data)
+            ->make(true);
+    }
+
+    public function addNewEmployee(Request $request){
+        $this->validate($request,[
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+        //Create new Item
+        $item = new User;
+        $item->name = $request->input('name');
+        $item->email = $request->input('email');
+        $item->password = $request->input('password');
+        $item->save();
+        return response($request->all());
+        // return redirect('/admin/employees');
+        //    return redirect('/items')->with('success','Success adding item');
+    }
+
+    public function updateEmployeeAccount(Request $request, $id){
+        $this->validate($request,[
+            'name' => 'required',
+            'email' => 'required',
+            //'password' => 'required',
+        ]);
+
+        $item = User::find($id);
+        $item->name = $request->input('name');
+        $item->email = $request->input('email');
+        $item->password = $request->input('password');
+        $item->save();
+        return response($request->all());
+    }
+    public function destroyEmployeeAccount($id){
+        $employee = User::find($id);
+        $employee->delete();
+        return redirect('/admin/employees');
+
+    }
+
+
+
+
+}
---
+++ edited/Jernixon/resources/views/adminViews/purchases.blade.php
@@ -0,0 +1,259 @@
+@extends('layouts.navbar')
+@section('purchases_link')
+class="active"
+@endsection
+
+@section('onload')
+onload="refresh()"
+@endsection
+
+@section('ng-app')
+ng-app="ourAngularJsApp"
+@endsection
+
+
+@section('headScript')
+<link href="{{asset('assets/css/datatables.min.css')}}" rel="stylesheet"/>
+<link href="{{asset('assets/css/buttons.dataTables.min.css')}}" rel="stylesheet"/>
+<!--AngularJs-->
+<script src="{{asset('assets/js/angular.min.js')}}"></script>
+<script>
+    function refresh(){
+        var len=localStorage.length;
+        var thatTbody = document.getElementById("cartTbody");
+        for(var i=0; i<len; i++) {
+            var key = localStorage.key(i);
+            var value = localStorage[key];
+            if(value.includes("item")){
+                // console.log(key + " => " + value);          
+                var myItemJSON = JSON.parse(localStorage.getItem(key));            
+                var newRow = thatTbody.insertRow(-1);
+                newRow.insertCell(-1).innerHTML = "<td>"+ myItemJSON.item +"</td>";
+                newRow.insertCell(-1).innerHTML = "<td>"+ myItemJSON.quantityLeft +"</td>";
+                newRow.insertCell(-1).innerHTML = "<td>"+ myItemJSON.wholeSalePrice +"</td>";
+                newRow.insertCell(-1).innerHTML = "<td>"+ myItemJSON.retailPrice +"</td>";
+                newRow.insertCell(-1).innerHTML = "<td><input type='number' value='" +myItemJSON.quantityPurchase+ "'min='1'></td>";
+                newRow.insertCell(-1).innerHTML = "<td></td>";
+                newRow.insertCell(-1).innerHTML = "<td><button class='btn btn-danger' data-item-id='" +myItemJSON.itemId+ "' onclick='remove(this)'>Remove</button></td>";
+
+            }
+        } 
+    }
+
+    function addItemToCart(button){
+        $(button).hide(500).delay(1000);
+        //$(button).removeClass("btn-info").addClass("btn-danger");
+        // $(button i).remove()
+        //.show(500);
+        //.html("<button class='btn btn-danger' onclick='addItemToCart(this)'>Remove</button>")
+
+        var data  = $(button.parentNode.parentNode.innerHTML).slice(0,-1);
+        if( localStorage.getItem(data[0].innerHTML) == null){
+            var thatTbody = document.getElementById("cartTbody");
+            var newRow = thatTbody.insertRow(-1);
+            //newTr.innerHTML = a.parentNode.parentNode.innerHTML ;
+            //thatTbody.append(newTr);
+            for(var i=0; i<data.length;i++){
+
+                newRow.insertCell(-1).innerHTML = data[i].innerHTML;
+            }
+            newRow.insertCell(-1).innerHTML = "<td><input class='ng-valid ng-valid-min ng-not-empty ng-dirty ng-valid-number ng-touched' type='number' value='1' min='1' ng-model='newQuantity' ng-change='myFunction()'></td>";
+            newRow.insertCell(-1).innerHTML ="<td ng-bind='" +data[0].innerHTML+"'></td>";
+            // newRow.insertCell(-1).innerHTML ="<td><h2 ng-bind='" +data[0].innerHTML+ "'></h2></td>";
+            newRow.insertCell(-1).innerHTML = "<td><button class='btn btn-danger' data-item-id='" +button.getAttribute("id")+ "' onclick='remove(this)'>Remove</button></td>";
+
+            //add to localStorage
+            var itemObject = {
+                item: data[0].innerHTML,
+                quantityLeft: data[2].innerHTML,
+                wholeSalePrice: data[3].innerHTML,
+                retailPrice: data[3].innerHTML,
+                quantityPurchase: "1",
+                //totalPrice:data[5].innerHTML,
+                itemId:button.getAttribute("id")
+
+            };
+
+            var jsonObject = JSON.stringify(itemObject);
+            localStorage.setItem(data[0].innerHTML,jsonObject);
+
+        }
+
+    }
+
+    function remove(button){
+        //var i = a.parentNode.parentNode.rowIndex;
+        //document.getElementById("cartTable").deleteRow(i);
+        var row = button.parentNode.parentNode; //row
+        $(row).hide(500,function(){
+            $(row).remove();
+        });
+
+        //remove item in local storage
+        var data  = $(button.parentNode.parentNode.innerHTML).slice(0,2);
+        localStorage.removeItem(data[0].innerHTML);
+
+        //show the plus sign button again
+        document.getElementById(button.getAttribute("data-item-id")).removeAttribute("style");
+    }
+
+    function addRow(){
+        var thatTbody = document.getElementById("purchasetable");
+        var newRow = thatTbody.insertRow(-1);
+        newRow.insertCell(-1).innerHTML = "<td><input class='ng-valid ng-valid-min ng-not-empty ng-dirty ng-valid-number ng-touched form-control' type='text' min='1'></td>";
+        newRow.insertCell(-1).innerHTML = "<td><input class='ng-valid ng-valid-min ng-not-empty ng-dirty ng-valid-number ng-touched form-control' type='number min='1'></td>";
+        newRow.insertCell(-1).innerHTML = "<td><input class='ng-valid ng-valid-min ng-not-empty ng-dirty ng-valid-number ng-touched form-control' type='number' min='1'></td>";
+        newRow.insertCell(-1).innerHTML = "<td><button class='btn btn-danger form-control' data-item-id=' +button.getAttribute('id')+ ' onclick='remove(this)'><i class='glyphicon glyphicon-remove'></i></button></td>";
+
+
+    }
+</script>
+
+@endsection
+
+@section('linkName')
+<h2>Purchases</h2>
+@endsection
+
+@section('right')
+<div class="row" >
+    <div class="col-md-12" >
+        <div class="card" >
+            <div class="header">
+                <div class="row">
+                    <div class="text-left">                                           
+                        <div class="col-md-12">
+                            <a href = "#create" data-toggle="modal" >
+                                <button type="submit" class="btn btn-info btn-fill btn-wd btn-success">Create Purchase Order</button> 
+                            </a> 
+                        </div>
+                    </div>
+                    <table class="table table-hover table-condensed" style="width:100%" id="dashboardDatatable">
+                        <thead> 
+                            <tr>
+                                <th>PO ID</th>
+                                <th>Date Created</th>
+                                <th>Supplier</th>
+                                <th>Status</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
+
+@section('modals')
+<div id="create" class="modal fade" tabindex="-1" role = "dialog" aria-labelledby = "viewLabel" aria-hidden="true">
+    <div class = "modal-dialog">
+        <div class = "modal-content">
+            <div class = "modal-body">
+                <button class="close" data-dismiss="modal">&times;</button>
+                <h4>Purchase Order</h4>
+                <div class="alert alert-danger hidden" id="errorDivAddNewItem">
+
+                </div>
+
+                {!! Form::open(['method'=>'post','id'=>'formPurchaseOrder']) !!}
+
+                <input type="hidden" id="_token" value="{{ csrf_token() }}">
+                <div class="form-group">
+                    <div class="row">
+                        <div class="col-md-3">
+                            {{Form::label('Date', 'Date:')}}
+                        </div>
+                        <div class="col-md-9">
+                            {{Form::text('Date','',['class'=>'form-control','placeholder'=>'Date'])}}
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group">                                
+                    <div class="row">
+                        <div class="col-md-3">
+                            {{Form::label('Official Receipt No:')}}
+                        </div>
+                        <div class="col-md-9">
+                            {{ Form::number('Official Receipt No','',['class'=>'form-control','placeholder'=>'Official Receipt No','min'=>'1']) }}
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group">    
+                    <div class="row">
+                        <div class="col-md-3">
+                            {{Form::label('Supplier', 'Supplier:')}}
+                        </div>
+                        <div class="col-md-9">
+                            {{Form::text('Supplier','',['class'=>'form-control','placeholder'=>'Supplier'])}}
+                        </div>
+                    </div>
+                </div>
+                <div class="row" >
+                    <div class="col-md-12" >
+                        <div class="card" >
+                            <div class="header">
+                                <div class="row">
+                                    <div class="text-left">                                           
+                                    </div>
+                                    <table class="table table-hover table-condensed" style="width:100%" id="">
+                                        <thead> 
+                                            <tr>
+
+                                                <th>Description</th>
+                                                <th>Quantity</th>
+                                                <th>Price</th>
+                                                <th>Action</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody id= "purchasetable">
+                                            <tr>
+                                                <td>{{Form::text('Description','',['class'=>'form-control'])}}</td>
+                                                <td>{{Form::number('Quantity','',['class'=>'form-control','min'=>'1'])}}</td>
+                                                <td>{{Form::text('Price','',['class'=>'form-control'])}}</td>
+                                                <td><button class='btn btn-danger form-control' data-item-id='" +button.getAttribute("id")+ "' onclick='removeRow(this)'><i class='glyphicon glyphicon-remove'></i></button></td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-12">
+                            <div class ="text-right">Total Items:</div>
+                            <div class ="text-right">Total Price:</div>
+                        </div>
+                    </div>
+                </div>                {!! Form::close() !!}
+
+                <button class="btn btn-info btn-fill btn-wd btn-success" onclick="addRow()">Add Row</button> 
+                <div class="row">
+                    <div class="text-right">                                           
+                        <div class="col-md-12">   
+                            <button id="submitNewItems" type="submit" onclick="window.alert('to be continue..')" class="btn btn-success">Save</button>
+                            <button class="btn btn-danger" data-dismiss="modal">Cancel</button>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+
+</div>
+
+
+@section('js_link')
+<!--   Core JS Files   -->
+{{--  <script src="{{asset('assets/js/jquery-1.10.2.js')}}"></script>  --}}
+<script src="{{asset('assets/js/jquery-1.12.4.js')}}"></script>
+<script src="{{asset('assets/js/bootstrap.min.js')}}"></script>
+<script src="{{asset('assets/js/jquery.dataTables.min.js')}}"></script>
+<script src="{{asset('assets/js/dataTables.buttons.min.js')}}"></script>
+
+
+@endsection

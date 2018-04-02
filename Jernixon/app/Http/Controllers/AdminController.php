9506f5e8fc21d5dbb26e779c1595f83475a43ef1
heycaesarjim <caesarromero1805@gmail.com>
2018-04-02 10:13:49 +0800

    ...

--- Jernixon/app/Http/Controllers/AdminController.php
+++ Jernixon/app/Http/Controllers/AdminController.php
@@ -379,7 +379,7 @@ class AdminController extends Controller
                         <button onclick='viewItemHistory(this)' class='btn btn-info'><i class='glyphicon glyphicon-th-list'></i> History</button>
                     </a>";
                 if($data->status === "available"){
-                   return $buttons."<button id='$data->product_id' onclick='formUpdateChangeStatus(this)' class='btn btn-danger'><i class='glyphicon glyphicon-remove'></i>Disable</button>";
+                   return $buttons."<button id='$data->product_id' onclick='formUpdateChangeStatus(this)' class='btn btn-danger'><i class='glyphicon glyphicon-remove'></i> Disable</button>";
                 }else{
                     return $buttons."<button id='$data->product_id' onclick='formUpdateChangeStatus(this)'class='btn btn-success'><i class='glyphicon glyphicon-ok'></i>Enable</button>";
                 }

4f8d3cd7fbd921ea145d336a7eff0b7991b0c5ff
marinelbaucas <marinelbaucas@gmail.com>
2018-05-23 17:39:54 +0800

    fixed and modified design

--- Jernixon/resources/views/notifications/Reorder_Notification.blade.php
+++ Jernixon/resources/views/notifications/Reorder_Notification.blade.php
@@ -1,16 +1,16 @@
 @if(!empty($notification->read_at) )
     <li class='list-group-item list-group-item-success'>
-        <p><b>Reorder Item:</b>  {{$notification->data['description']}}</p>
+        <h6><b>Reorder Item:</b>  {{$notification->data['description']}}</h6>
         
-        <p><b>Remaining Quantity:</b> {{$notification->data['quantity']}}
-        </p>         
+        <h6><b>Remaining Quantity:</b> {{$notification->data['quantity']}}
+        </h6>         
     </li>
 @else       
     <li class='list-group-item list-group-item-danger'>
-        <p><b>Reorder Item:</b>  {{$notification->data['description']}}</p>
+        <h6><b>Reorder Item:</b>  {{$notification->data['description']}}</h6>
         
-        <p><b>Remaining Quantity:</b> {{$notification->data['quantity']}}
-        </p>                              
-        <a href="/markAsRead/{{$notification->id}}" style="color:black;"><button >Mark As Read</button></a>
+        <h6><b>Remaining Quantity:</b> {{$notification->data['quantity']}}
+        </h6>                              
+        <a href="/markAsRead/{{$notification->id}}" style="color:black;"><button class = "btn btn-success">Mark As Read</button></a>
     </li>
 @endif
--- Jernixon/resources/views/notifications/stock_adjustment_notification.blade.php
+++ Jernixon/resources/views/notifications/stock_adjustment_notification.blade.php
@@ -1,16 +1,16 @@
 @if(!empty($notification->read_at) )
     <li class='list-group-item list-group-item-success'>
-        <p><b>Item Name:</b>  {{$notification->data['itemname']}}</p>
-        <p><b>Quantity:</b>  {{$notification->data['quantity']}}</p>
-        <p><b>Status:</b> {{$notification->data['status']}}</p>
-        <p><b>User:</b> {{$notification->data['user']}}</p>         
+        <h6><b>Item Name:</b>  {{$notification->data['itemname']}}</h6>
+        <h6><b>Quantity:</b>  {{$notification->data['quantity']}}</h6>
+        <h6><b>Status:</b> {{$notification->data['status']}}</h6>
+        <h6><b>User:</b> {{$notification->data['user']}}</h6>         
     </li>
 @else       
     <li class='list-group-item list-group-item-danger'>
-        <p><b>Item Name:</b>  {{$notification->data['itemname']}}</p>
-        <p><b>Quantity:</b>  {{$notification->data['quantity']}}</p>
-        <p><b>Status:</b> {{$notification->data['status']}}</p>
-        <p><b>User:</b> {{$notification->data['user']}}</p>   
-        <a href="/markAsRead/{{$notification->id}}" style="color:black;"><button >Mark As Read</button></a>
+        <h6><b>Item Name:</b>  {{$notification->data['itemname']}}</h6>
+        <h6><b>Quantity:</b>  {{$notification->data['quantity']}}</h6>
+        <h6><b>Status:</b> {{$notification->data['status']}}</h6>
+        <h6><b>User:</b> {{$notification->data['user']}}</h6>   
+        <a href="/markAsRead/{{$notification->id}}" style="color:black;"><button class = "btn btn-success">Mark As Read</button></a>
     </li>
 @endif

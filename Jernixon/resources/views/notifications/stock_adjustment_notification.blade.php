b826c3035d291acb47ad8ce268ca0fb5990d2303
engelyncarreon <engelyn.carreon@gmail.com>
2018-05-30 12:33:09 +0800

    edited the design of stock_adjustment_notification.blade.php

--- Jernixon/resources/views/notifications/stock_adjustment_notification.blade.php
+++ Jernixon/resources/views/notifications/stock_adjustment_notification.blade.php
@@ -1,16 +1,17 @@
 @if(!empty($notification->read_at) )
-    <li class='list-group-item list-group-item-success'>
+    <li class='list-group-item list-group-item-success hidden readNotif'>
         <h6><b>Item Name:</b>  {{$notification->data['itemname']}}</h6>
         <h6><b>Quantity:</b>  {{$notification->data['quantity']}}</h6>
         <h6><b>Status:</b> {{$notification->data['status']}}</h6>
         <h6><b>User:</b> {{$notification->data['user']}}</h6>         
     </li>
 @else       
-    <li class='list-group-item list-group-item-danger'>
-        <h6><b>Item Name:</b>  {{$notification->data['itemname']}}</h6>
-        <h6><b>Quantity:</b>  {{$notification->data['quantity']}}</h6>
-        <h6><b>Status:</b> {{$notification->data['status']}}</h6>
-        <h6><b>User:</b> {{$notification->data['user']}}</h6>   
-        <a href="/markAsRead/{{$notification->id}}" style="color:black;"><button class = "btn btn-success">Mark As Read</button></a>
-    </li>
+        <li class='list-group-item list-group-item-danger hidden unreadstock'>
+            <h6><b>Item Name:</b>  {{$notification->data['itemname']}}</h6>
+            <h6><b>Quantity:</b>  {{$notification->data['quantity']}}</h6>
+            <h6><b>Status:</b> {{$notification->data['status']}}</h6>
+            <h6><b>User:</b> {{$notification->data['user']}}</h6>   
+            <a href="/markAsRead/{{$notification->id}}" style="color:black;"><button class = "btn btn-success">Mark As Read</button></a>
+        </li>
 @endif
+
